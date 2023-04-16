acf576aec4d21ad60b482183e6d39132b60adfad
nolleh7707 <nolleh7707@gmail.com>
2023-04-16 10:53:14 +0900

    fix: missed file add

--- src/gh-deploy.ts
+++ src/gh-deploy.ts
@@ -1,11 +1,29 @@
 import { ActionInterface, Status } from "./constants";
-import { ssh } from "./util";
+import {
+  ssh,
+  generateFolderPath,
+  checkParameters,
+  generateRepositoryPath,
+  generateTokenType,
+} from "./util";
 import { deploy, init } from "./git";
+
 export async function githubDeploy(settings: ActionInterface): Promise<Status> {
+  // Defines the repository/folder paths and token types.
+  // Also verifies that the action has all of the required parameters.
+  settings.folderPath = generateFolderPath(settings);
+
+  checkParameters(settings);
+
+  settings.repositoryPath = generateRepositoryPath(settings);
+  settings.tokenType = generateTokenType(settings);
+
   if (settings.sshKey) {
     await ssh(settings);
   }
 
+  // Defines the repository/folder paths and token types.
+  // Also verifies that the action has all of the required parameters.
   await init(settings);
   return await deploy(settings);
 }
